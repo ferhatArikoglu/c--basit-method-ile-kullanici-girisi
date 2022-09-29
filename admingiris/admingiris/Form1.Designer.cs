19f9c7ce7baac697e92708ca4bcc7216a8cc67f0
Ferhat Arıkoğlu <34ferhatarikoglu34@gmail.com>
2022-09-29 19:31:17 +0300

    c# method ile basit kullanıcı girişi

---
+++ admingiris/admingiris/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace admingiris
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtkadi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtsifre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtkadi
+            // 
+            this.txtkadi.Location = new System.Drawing.Point(332, 123);
+            this.txtkadi.Name = "txtkadi";
+            this.txtkadi.Size = new System.Drawing.Size(181, 27);
+            this.txtkadi.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "kullanıcı adı";
+            // 
+            // txtsifre
+            // 
+            this.txtsifre.Location = new System.Drawing.Point(332, 162);
+            this.txtsifre.Name = "txtsifre";
+            this.txtsifre.PasswordChar = '*';
+            this.txtsifre.Size = new System.Drawing.Size(181, 27);
+            this.txtsifre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "şifre";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(419, 299);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(56, 18);
+            this.lbl1.TabIndex = 5;
+            this.lbl1.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 405);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtsifre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtkadi);
+            this.Enabled = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtkadi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtsifre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ admingiris/admingiris/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace admingiris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //static dersek dışarıdaki şeyleri içeri dahil edemiyoruz ancak parametre ile alabiliriz;))
+        static  void kullanıcıkontrol(string kadi,string sifre)
+        {
+            if (kadi == "1" && sifre == "1")
+            {
+                MessageBox.Show("giriş başarılı");
+            }
+            else
+            {
+                MessageBox.Show("yanlış şifre girdiniz");
+            }
+                
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kullanıcıkontrol(txtkadi.Text, txtsifre.Text);
+        }
+    }
+}
